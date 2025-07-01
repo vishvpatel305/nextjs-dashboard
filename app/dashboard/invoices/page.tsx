a43b3e44cf106409513a5d12102973dd58ddeedb
vishv patel <vishvpatel1122@gmail.com>
2025-07-01 19:01:56 +0530

    Fix searchParams typing issue

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,16 +7,14 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default async function Page(props: {
-	searchParams?: Promise<{
-		query?: string;
-		page?: string;
-	}>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
 }) {
-	const searchParams = await props.searchParams;
-	const query = searchParams?.query || '';
-	const currentPage = Number(searchParams?.page) || 1;
-	const totalPages = await fetchInvoicesPages(query);
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
